972d528547be9d31282475a2e3d3176313d2248b
Emilian Losneanu <emilian.losneanu@cegeka.com>
2016-04-14 13:53:28 +0300

    add install brew tools

---
+++ install-brew-cask-tools.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set
+-e
+
+adium \
+alfred \
+android-file-transfer \
+atom \
+bitbar \
+brackets \
+dockertoolbox \
+dropbox \
+fantastical \
+firefoxdeveloperedition \
+flux \
+folx \
+gfxcardstatus \
+gitkraken \
+gog-galaxy \
+google-chrome \
+horndis \
+intellij-idea \
+istat-menus \
+iterm2 \
+java \
+java7 \
+jitsi \
+kdiff3 \
+keycastr \
+kodi \
+launchcontrol \
+microsoft-lync \
+mongo-management-studio \
+mplayerx \
+nylas-n1 \
+obs \
+pocketcast \
+skype \
+slack \
+smcfancontrol \
+soapui \
+spectacle \
+spotify \
+sublime-text3 \
+teamviewer \
+textmate \
+tunnelbear \
+unity \
+vagrant \
+virtualbox \
+vox \
+xmind
